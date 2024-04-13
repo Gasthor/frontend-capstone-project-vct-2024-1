e78114d709cb25b30cc84f4b7aefac06aa6889bc
Gaston <64218013+Gasthor@users.noreply.github.com>
2024-04-13 17:53:59 -0400

    [Change] value of separations in container and style rules in navbar

--- components/UI/Navbar/Navbar.tsx
+++ components/UI/Navbar/Navbar.tsx
@@ -2,17 +2,17 @@ import Image from 'next/image'
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-evenly p-2 border-b-2 border-orange-500 shadow-md w-full">
-           <div>
-           <Image alt='Logo centro de investigacion e innovacion' src="/logo-CII.svg" width={160} height={12}/>
-           </div>
-        <div>
-        <Image alt='Logo Viña Concha y Toro' src="/logo-vina.svg" width={300} height={12}/>
-        </div>
-    <div>
-    <Image alt='Logo Viña Concha y Toro' src="/logo-ucn.png" width={60} height={12}/>
-    </div>
-            
+        <nav className="flex justify-around mb-8 p-2 border-b-2 border-orange-500 shadow-md w-full bg-white">
+            <div>
+                <Image alt='Logo centro de investigacion e innovacion' src="/logo-CII.svg" width={160} height={12} />
+            </div>
+            <div>
+                <Image alt='Logo Viña Concha y Toro' src="/logo-vina.svg" width={300} height={12} />
+            </div>
+            <div>
+                <Image alt='Logo UCN' src="/logo-ucn.png" width={60} height={12} />
+            </div>
+
         </nav>
     )
 }
