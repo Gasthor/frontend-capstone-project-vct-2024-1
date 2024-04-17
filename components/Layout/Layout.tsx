f54704d703350a9d23ef2381fc6649af2065d441
Gaston <64218013+Gasthor@users.noreply.github.com>
2024-04-16 20:19:48 -0400

    [ADD] toasts for alerts, axios for requests

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,26 +2,29 @@ import Head from "next/head"
 import { FC } from "react"
 import Navbar from "../UI/Navbar/Navbar"
 import Footer from "../UI/Footer/Footer"
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 interface Props {
-    children : React.ReactNode | React.ReactNode[]
-    title : string
+    children: React.ReactNode | React.ReactNode[]
+    title: string
     pageDescription: string
 }
 export const Layout: FC<Props> = ({
     children, title, pageDescription
 }) => {
-    return(
+    return (
         <div className="flex flex-col min-h-screen">
-        <Head>
-            <title>{title}</title>
-            <meta name="description" content={pageDescription}/>
-        </Head>
-        <Navbar/>
-        <main className="flex flex-col gap-4 sm:gap-8 items-center p-4 flex-grow">
-            {children}
-        </main>
-        <Footer/>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={pageDescription} />
+            </Head>
+            <Navbar />
+            <main className="flex flex-col gap-4 sm:gap-8 items-center p-4 flex-grow">
+                {children}
+                <ToastContainer />
+            </main>
+            <Footer />
         </div>
     )
 }
